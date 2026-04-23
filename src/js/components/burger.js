4aea559c07c176370da08a51d583e925075f2003
Egor <aap.porno228.iorio@mail.ru>
2026-04-23 23:13:14 +0300

    Created hero section

--- src/js/components/burger.js
+++ src/js/components/burger.js
@@ -1,19 +1,25 @@
 export const useBurger = () => {
   const burgerBtn = document.querySelector('[data-burger="btn"]');
-
   const overlay = document.querySelector('[data-burger="overlay"]');
   const burgerMenu = document.querySelector('[data-burger="menu"]');
+  const body = document.body;
+
+  if (!burgerBtn || !overlay || !burgerMenu || !body) {
+    return;
+  }
 
   const closeBurgerMenu = () => {
     burgerBtn.classList.remove('burger--active');
-    overlay.classList.remove('header__right--visible');
+    burgerMenu.classList.remove('header__right--visible');
     overlay.classList.remove('overlay--visible');
+    body.classList.remove('body--fixed');
   };
 
   burgerBtn.addEventListener('click', () => {
     burgerBtn.classList.toggle('burger--active');
     burgerMenu.classList.toggle('header__right--visible');
     overlay.classList.toggle('overlay--visible');
+    body.classList.toggle('body--fixed');
   });
 
   overlay.addEventListener('click', closeBurgerMenu);
