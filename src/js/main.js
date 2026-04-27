8e38ac7fee673b741086a2a4a801b805ef15e91f
Egor <aap.porno228.iorio@mail.ru>
2026-04-27 14:40:39 +0300

    Created insight

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,8 @@ import '../assets/scss/Main.scss';
 
 import { useTheme } from './components/theme.js';
 import { useBurger } from './components/burger.js';
+import { useInsightSlider } from './components/swiper.js';
 
 useTheme();
 useBurger();
+useInsightSlider();
