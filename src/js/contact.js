06174b0c1f9ffa94d0cee15352744ff811711cfb
Твоё Имя <_@example.com>
2026-05-07 22:52:50 +0300

    Created contact

--- src/js/contact.js
+++ src/js/contact.js
@@ -2,6 +2,8 @@ import '../assets/scss/contact.scss';
 
 import { useTheme } from './components/theme.js';
 import { useBurger } from './components/burger.js';
+import { usePhone } from './components/contact/phone.js';
 
 useTheme();
 useBurger();
+usePhone();
